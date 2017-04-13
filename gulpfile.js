65e684dabf2c51bf9bee84bfac70a0e5e563886d
avmaktslave <4tharraofdagon@gmail.com>
2017-04-13 17:15:27 +0300

    some changes in less 10, continua leter

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
 gulp.task('browser-sync', function() {
   browserSync.init({
     server: {
-      baseDir: 'css_4e/09/nav_bar'
+      baseDir: 'css_4e/10'
     }
   });
 });
